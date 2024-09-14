627409391bdd1b398712e3be1d34059fad5f16a4
HnaTeiV <hpk1305@gmail.com>
2024-09-15 00:35:56 +0700

    "Connect Database"

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VA_EcommerceWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
